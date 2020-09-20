5d6c0b4a9004f51500aa359332878cf9114d7d29
Islam Zaripov <islam.zar@yandex.ru>
2020-09-20 23:29:57 +0300

    Database support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from 'auth/auth.controller';
-import { AuthModule } from 'auth/auth.module';
+import {AuthModule} from "auth/auth.module";
 import { UsersModule } from 'users/users.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule,
+    TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [],
 })
