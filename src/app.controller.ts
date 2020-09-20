018d7cd8c341ca30806a268b8f2481c49e9ab485
Islam Zaripov <islam.zar@yandex.ru>
2020-09-20 11:12:15 +0300

    Initial

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+
+    constructor(private readonly appService: AppService) {}
+
+    @Get()
+    getHello(): string {
+        //throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+        return this.appService.getHello();
+    }
+}
